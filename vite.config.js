475225bfc4eca3a7d1077326852a261a8ad4bebf
L4RS3N <94544098+4NRICKL4RS3N@users.noreply.github.com>
2024-09-22 11:36:03 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/sass/app.scss',
-                    'resources/sass/page/_accueil.scss',
                     'resources/js/app.js'
             ],
             refresh: true,
