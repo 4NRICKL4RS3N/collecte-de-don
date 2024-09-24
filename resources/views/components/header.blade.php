e3e760809ffeb712ac872964fc0fe9e03fcc5c51
L4RS3N <94544098+4NRICKL4RS3N@users.noreply.github.com>
2024-09-24 09:35:10 +0300

    finishing project card

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -16,13 +16,13 @@
                 <li class="nav-item mx-sm-2 mx-lg-4 {{ Request::is('accueil') ? '' : 'menu-lien' }}">
                     <a class="nav-link {{ Request::is('accueil') ? 'bold-text' : '' }}" href="/accueil">Accueil</a>
                 </li>
-                <li class="nav-item mx-sm-2 mx-lg-4 menu-lien {{ Request::is('a-propos') ? '' : 'menu-lien' }}">
+                <li class="nav-item mx-sm-2 mx-lg-4 {{ Request::is('a-propos') ? '' : 'menu-lien' }}">
                     <a class="nav-link {{ Request::is('a-propos') ? 'bold-text' : '' }}" href="/a-propos">À propos</a>
                 </li>
-                <li class="nav-item mx-sm-2 mx-lg-4 menu-lien {{ Request::is('projets') ? '' : 'menu-lien' }}">
+                <li class="nav-item mx-sm-2 mx-lg-4 {{ Request::is('projets') ? '' : 'menu-lien' }}">
                     <a class="nav-link {{ Request::is('projets') ? 'bold-text' : '' }}" href="/projets">Projets</a>
                 </li>
-                <li class="nav-item mx-sm-2 mx-lg-4 menu-lien {{ Request::is('contact') ? '' : 'menu-lien' }}">
+                <li class="nav-item mx-sm-2 mx-lg-4 {{ Request::is('contact') ? '' : 'menu-lien' }}">
                     <a class="nav-link {{ Request::is('contact') ? 'bold-text' : '' }}" href="/contact">Contact</a>
                 </li>
             </ul>
