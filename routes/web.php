0646028848d9237c581e06e7e1a340a27dc6c40d
L4RS3N <94544098+4NRICKL4RS3N@users.noreply.github.com>
2024-10-01 14:54:16 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/projets/titre', function () {
 });
 
 Route::get('/donate', [DonationController::class, 'index'])->name('donate.afficher');
-Route::post('/create-payment-intent', [DonationController::class, 'createPaymentIntent']);
+Route::post('/create-payment-intent', [DonationController::class, 'createPaymentIntent'])->name('createPaymentIntent');
 Route::post('/confirm-payment', [DonationController::class, 'process'])->name('confirmPayment');
 Route::get('/donate/thank-you', [DonationController::class, 'thankYou'])->name('donate.thank-you');
 Route::post('/stripe-webhook', [DonationController::class, 'handleWebhook']);
