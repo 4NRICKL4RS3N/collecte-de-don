44e2c317c6b3c0a81bd009202b087122a06c8714
4NR1CKL4RS3N <94544098+4NRICKL4RS3N@users.noreply.github.com>
2024-10-08 10:42:20 +0300

    Update accueil.blade.php

--- resources/views/client/pages/accueil.blade.php
+++ resources/views/client/pages/accueil.blade.php
@@ -11,7 +11,7 @@
                     <div class="col-md-8 offset-md-2">
                         <h1 class="display-4 grand-titre"><span class="bold-text">Participez</span> à notre mission
                             d'évangélisation</h1>
-                        <x-client.button add-class="btn-primary" lien="/donate" content="Faites une différence aujourd'hui"/>
+                        <x-button add-class="btn-primary" lien="/donate" content="Faites une différence aujourd'hui"/>
                     </div>
                 </div>
             </div>
